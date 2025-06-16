fb5355bb8ee35cc2c2dd70ba7f7335c028ae5ea0
MercuryDemon <nikita190400@gmail.com>
2025-06-16 22:52:59 +0300

    func "after" - done

--- concurrency/easy/time-after.go
+++ concurrency/easy/time-after.go
@@ -10,13 +10,10 @@ import (
 // возвращает канал, в котором появится значение      // какое значение будет передаваться я тоже не понял нихуя
 // через промежуток времени dur
 func after(dur time.Duration) <-chan time.Time {
-	var timeout = make(chan time.Time)  //Создаю канал, который буду возвращать
-	defer close(timeout)		    //На всякий случай закрываю его по зевершению функции
-	
-	var myTime time.Time		   //Создал переменную того типа который можно прокинуть по каналу
-	time.Sleep(dur * time.Second       //я думаю что сплю 10 сек здесь? но мне кажется это нихуя не так работает
-	timeout<- myTime		   //Прокидываю по каналу переменную которую можно прокинуть
-	return timeout			  //Возвращаю канал
+	var timeout <-chan time.Time		// Создаю пиздец какой опасный nil chanal
+	timeoutDur := time.NewTimer(dur * time.Second)		// ставлю таймер на dur в секундах
+	timeout = timeoutDur.C		//передаю сигнал в канал 
+	return timeout	  //Возвращаю канал
 }
 
 func withTimeout(fn func() int, timeout time.Duration) (int, error) {
